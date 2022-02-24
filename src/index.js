5b21ee6d8de73beb25050ed83cddc11d633a68ca
imbickydutta <imbickydutta@gmail.com>
2022-02-24 21:33:02 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connect = require('./config/db');
+require("dotenv").config();
 
 const userController = require('./controllers/user.controller');
 const fileController = require('./controllers/file.controller');
@@ -12,8 +13,9 @@ app.use(express.json());
 app.use("/user", userController);
 app.use("/drive", fileController);
 
+let port = process.env.PORT || 1698;
 
-app.listen(1698, async () => {
+app.listen(port, async () => {
     try {
         await connect();
         console.log("Server is running on port 1698");
